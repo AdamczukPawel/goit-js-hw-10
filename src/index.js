5625700e5190429b10f7806384abf2d07cfe3d6d
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-10-23 22:56:21 +0200

    Napisano kod który pobiera informacje w zależności od tego co zostanie wpisane do search-box

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import { debounce } from 'lodash';
+import { fetchCountries } from './fetchCountries.js';
+
+const searchBox = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
 
 const DEBOUNCE_DELAY = 300;
+
+// function showAtLeastTwo() {
+
+// }
+
+function searchAndShowCountries(event) {
+    const inputedValue = event.target.value.trim();
+    countryList.innerHTML = "";
+    countryInfo.innerHTML = "";
+    if (inputedValue.length !== 0) {
+        fetchCountries(inputedValue)
+        .then(data => console.log(data))
+    }
+};
+
+searchBox.addEventListener('input', debounce(searchAndShowCountries, DEBOUNCE_DELAY))
