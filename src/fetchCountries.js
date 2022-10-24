c601f06c85b197da4a8ef43160616f7ac3f8f173
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-10-24 20:53:10 +0200

    Ukończenie przedostatniej pracy domowej z JS

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
 const URL_API_REST_COUNTRIES = 'https://restcountries.com/v3.1/name/';
 
 function fetchCountries(name) {
-    return fetch(URL_API_REST_COUNTRIES + name + '?fields=name.official,capital,population,flags.svg,languages')
+    return fetch(URL_API_REST_COUNTRIES + name + '?fields=name,capital,population,flags,languages')
         .then(response => response.json())
 };
 
--- src/index.js
+++ src/index.js
@@ -9,9 +9,29 @@ const countryInfo = document.querySelector(".country-info");
 
 const DEBOUNCE_DELAY = 300;
 
-// function showAtLeastTwo() {
+function showMatchingCountries(country) {
+    const markup = country
+    .map(({ name, flags }) => {
+        return `<li class="country-list-element">
+            <img src = ${flags.svg} alt=${ name.common }'s flag' width=30px height=20px/>
+            ${name.common}
+        </li>`;
+    })
+    .join("");
+  countryList.insertAdjacentHTML('afterbegin', markup);
+}
 
-// }
+function showMatchingCountryInformations(country) {
+    const markupInfo = country.map(({name, flags, capital, languages, population}) => {
+        return `<h1><img class="big-flag" src=${flags.svg} alt=${name.common}'s flag' width=50px height=30px/>${name.common}</h1>
+        <ul>
+        <li class="coutry-data"><span class="coutry-data-type">Capital: </span>${capital}</li>
+        <li class="coutry-data"><span class="coutry-data-type">Population: </span>${population}</li>
+        <li class="coutry-data"><span class="coutry-data-type">Languages: </span>${Object.values(languages)}</li>
+        </ul>`
+    })
+    countryInfo.insertAdjacentHTML('afterbegin', markupInfo);
+}
 
 function searchAndShowCountries(event) {
     const inputedValue = event.target.value.trim();
@@ -19,7 +39,19 @@ function searchAndShowCountries(event) {
     countryInfo.innerHTML = "";
     if (inputedValue.length !== 0) {
         fetchCountries(inputedValue)
-        .then(data => console.log(data))
+            .then(data => {
+                if (data.status === 404) {
+                    Notiflix.Notify.failure("Oops, there is no country with that name");
+                } else {
+                    if (data.length > 10) {
+                        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+                    } else if (data.length >= 2 && data.length <= 10) {
+                        showMatchingCountries(data)
+                    } else if (data.length = 1) {
+                        showMatchingCountryInformations(data)
+                    }
+                }
+})
     }
 };
 
